8dc618430abe2bf134a84c0b0b43d034aa4d5512
cloud-wu <cloud77920@gmail.com>
2018-03-31 19:08:26 +0800

    task 的新增/刪除功能 - 前端畫面（尚未與後端串接資料）

--- resources/assets/js/views/Task.vue
+++ resources/assets/js/views/Task.vue
@@ -9,7 +9,7 @@
                 <div class="panel-body">
 
                     <!-- New Task Form -->
-                    <form action="" method="POST" class="form-horizontal">
+                    <form @submit.prevent="add" class="form-horizontal">
 
                         <!-- Task Name -->
                         <div class="form-group">
@@ -46,15 +46,13 @@
                         </thead>
                         <tbody>
                         <tr v-for="task in view.tasks" :key="task.id">
-                            <td class="table-text"><div>task.name</div></td>
+                            <td class="table-text"><div>{{ task.name }}</div></td>
 
                             <!-- Task Delete Button -->
                             <td>
-                                <form action="" method="POST">
-                                    <button type="submit" class="btn btn-danger">
-                                        <i class="fa fa-btn fa-trash"></i>Delete
-                                    </button>
-                                </form>
+                                <button @click.prevent.stop="remove(task.id)" type="submit" class="btn btn-danger">
+                                    <i class="fa fa-btn fa-trash"></i>Delete
+                                </button>
                             </td>
                         </tr>
                         </tbody>
@@ -70,6 +68,7 @@
     export default {
         data () {
             return {
+                currentId: 3,
                 form: {
                     task: ''
                 },
@@ -85,6 +84,23 @@
         computed: {
             hasTasks () {
                 return this.view.tasks.length > 0;
+            }
+        },
+        methods: {
+            add () {
+                if (this.form.task === '') {
+                    return false;
+                }
+
+                this.currentId++;
+                this.view.tasks.push({ id: this.currentId, name: this.form.task });
+                this.resetForm();
+            },
+            remove (id) {
+                this.view.tasks = this.view.tasks.filter(task => task.id !== id);
+            },
+            resetForm () {
+                this.form.task = '';
             }
         }
     }
